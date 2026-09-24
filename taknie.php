1035fc7f5217da196eb0258cce6abb441dd57202
Mocchi <mikolajgolonka43@gmail.com>
2026-09-24 14:04:11 +0200

    Create taknie.php

---
+++ taknie.php
@@ -0,0 +1,17 @@
+<?php
+
+class Prostokat {
+    public $a;
+    public $b;
+
+    public function area($a, $b){
+        return $a * $b;
+    }
+
+    public function pirimiter($a,$b){
+        echo "<br>"
+        
+    }
+}
+
+$wyniki = new Prostokat
